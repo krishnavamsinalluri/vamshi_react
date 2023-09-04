9b34b6046e3931b25af7bd80e91aaf75eed95087
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-09-04 19:17:47 +0530

    bnj

--- src/Food.jsx
+++ src/Food.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from"axios";
 
 function Food(){
-    axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood").then(function(res){
-        console.log()
+    var[items,setItems]=React.useState([])
+  useEffect(function(){
+        axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood").then(function(res){
+        console.log(res.data.meal)
+        setItems([...res.data.meal])
     })
+    },[])
+    
 
 
 
     return(
         <div>
+            {
+                items.map((s)=>{
+                    return(
+                        <li>{s.meal}</li>
+                    )
+                })
+            }
 
         </div>
     )
