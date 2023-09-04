dbba0b0f62cfccb66f26366f7354a5598f250206
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-09-04 12:33:08 +0530

    ss

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import { AiOutlineMinus, AiOutlinePlus } from 'bootstrap/dist/css/bootstrap.min.css';
 var Answer=({title,info})=>{
 var[showdata,setShowdata]=React.useState(false)
     return(
         <div>
             <h1>{title}</h1>
            < button className='btn' onClick={() => setShowdata(!showdata)}>
-          {showdata ? <AiOutlineMinus /> : <AiOutlinePlus />}
+          {showdata ?<AiOutlineMinus/>:<AiOutlineMinus/> }
         </button>
         {showdata &&<p>{info}</p> }
 
