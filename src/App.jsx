392cdcd52a02aeeed7a32fa993d3d868dae2a942
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-09-09 12:48:03 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,21 @@ function App() {
     <div className="box">
       <img style={{ width: "10rem" }} src="https://www.ammammapodi.com/assets/frontend/images/ammaicon.png" alt="" />
       <div className="items">
-        <Link to="/aboutus">About us</Link>
+       <button>  <Link to="/aboutus">About us</Link>
+            </button>       &nbsp;&nbsp;&nbsp;
+        <button><Link to="/contactus">Contact us</Link>
+        </button>
         &nbsp;&nbsp;&nbsp;
-        <Link to="/contactus">Contact us</Link>
+        <button> <Link to="/sweetitems">Sweeat iteams</Link>
+              </button>
         &nbsp;&nbsp;&nbsp;
-        <Link to="/sweetitems">Sweeat iteams</Link>
-        &nbsp;&nbsp;&nbsp;
-        <Link to="/hotitems">Hot iteams</Link>
+            <button> <Link to="/hotitems">Hot iteams</Link>
+           </button>
+
       </div>
+      <iframe width="1230" height="300" src="https://www.youtube.com/embed/odCMxmQv6WA" title="Eggless Flour Sweets Snacks | Fried Flour Sweet Snacks | Easy Sweet Snacks | N&#39;Oven" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       <div className="card">
         <Outlet>
-        <iframe width="688" height="387" src="https://www.youtube.com/embed/odCMxmQv6WA" title="Eggless Flour Sweets Snacks | Fried Flour Sweet Snacks | Easy Sweet Snacks | N&#39;Oven" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
         </Outlet>
         
--- src/Contactus.jsx
+++ src/Contactus.jsx
@@ -6,11 +6,12 @@ function Countact(){
     return(
         <div className="card">  
         <img style={{width:"18rem"}} src="https://www.ammammapodi.com/assets/frontend/images/ammaicon.png" alt="" />  
+        
             
              <div className="card-body">
              <i className="bi bi-telephone-inbound-fill">9573642409</i><br />
              <i className="bi bi-envelope-at-fill">support@ammaammagarifood</i> <br />
-            <i className="bi bi-house-add-fill">Kukatpally,hemadri residency ,cake factory road,Pin code:500072</i>
+            <i className="bi bi-house-add-fill">Kukatpally,hemadri residency ,cake factory road,Pin code:500072</i><br />
             <i className="bi bi-bag-check-fill"> 24/7 Available</i>
 
         </div>
