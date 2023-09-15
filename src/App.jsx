b361d2483fd8a958bf420cca691523022a2adcfb
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-09-15 12:21:59 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,8 @@ function App(props) {
 
   return (
     <div className="box">
-      <h1>India vs Praskisan</h1>
+      <h1>HELLO</h1>
       
-      <Counter s={0} i={6}></Counter>
-      <br/>
-      <Counter s={0} i={4}></Counter>
-      <br />
-      <Todolist></Todolist>
 
     
     </div>
