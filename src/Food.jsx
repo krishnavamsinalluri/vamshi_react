e232a332879cd6b2828ac4512a547e0d5928ce5c
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-10-10 14:28:14 +0530

    ss

--- src/Food.jsx
+++ src/Food.jsx
@@ -5,8 +5,8 @@ function Food(){
     var[items,setItems]=React.useState([])
   React.useEffect(function(){
         axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood").then(function(res){
-        console.log(res.data.meal)
-        setItems([...res.data.meal])
+        console.log(res)
+        setItems([...res])
     })
     },[])
     
@@ -18,7 +18,7 @@ function Food(){
             {
                 items.map((s)=>{
                     return(
-                        <li></li>
+                        <li>{s}</li>
                     )
                 })
             }
