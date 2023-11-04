6788facacb7d80f93f968aa215cd7c5f07ce4449
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-11-04 16:23:40 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Inceriment from "./inceriment";
+import Todo from "./Todolist";
 function App(){
   return (
     <div>
       <Inceriment></Inceriment>
+      <Todo></Todo>
     </div>
   )
 
--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 function Todo(){
+    var[players,setPlayers]=React.useState([])
+    function ab(){
+      
+        var x=document.getElementById("dd").value
+        setPlayers([...players,x])
+    }
+        function abc(i){
+            var tem=players
+            tem.splice(i,1)
+            setPlayers([...players])
+
+        }
+
+
 
     return (
-        <div>
-            
+        <div className='box'>
+            <input type="text" id='dd' />
+            <button onClick={ab}>add todo</button>
+            {
+            players.map((a,i)=>{
+                return <li>{a}
+                <button onClick={abc}>detele</button></li>
+            })
+            }
         </div>
     )
 }
