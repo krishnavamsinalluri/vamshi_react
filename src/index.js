32c516b6c94d01f674a6b79cd40244679c45dad0
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-11-29 10:59:21 +0530

    ss

--- src/index.js
+++ src/index.js
@@ -2,10 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Country from './Restcountries';
+import Data from './Countrys';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children:[{
+      path:"/countrydetalis",
+      element:<Country></Country>
+    },
+    {
+      path:"/country",
+      element:<Data></Data>
+    }
+
+    ]
+    
+  }
+
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <RouterProvider router={router} />
+)
+
+
 
 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
