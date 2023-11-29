0d908a0062f8c8204060bd92a2ca49b8e2949f9b
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-11-29 11:47:28 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@
 import './App.css';
 import Country from './Restcountries'
 import Data from './Countrys';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
   return (<div className='box' id="dd">
     <h1>Countries</h1>
-    <Country></Country>
-    <Data></Data>
-    </div>
+      <Link to="/countrydetalis">Coutry details</Link>&nbsp;
+      <Link to="/country">countrys</Link>
+
+      <div>
+        <Outlet></Outlet>
+        
+      </div>
+  </div>
   );
 }
 
--- src/Countrys.jsx
+++ src/Countrys.jsx
@@ -3,18 +3,23 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 function Data(){
 var [rest,setRest]=React.useState([])
-    React.useEffect(()=>{
+    React.useEffect(function(){
         axios.get("https://restcountries.com/v3/all").then(function(res){
-            console.log(res)
+            console.log(res.data)
+            setRest([...res.data])
 
         })
     },[])
 
     return(
-        <div>
+        <div className="box">
             {
-                rest.map((a,i)=>{
-                    return(<li>{a}</li>)
+             rest.length>0 && rest.map((a,i)=>{
+                    return(<div className="main">
+                        <li>{a.name.common}</li>
+                        <img style={{width:"100px"}} src={a.flags[0]} alt="" />
+                        <p>{a.population}</p>
+                    </div>)
                 })
             }
         </div>
