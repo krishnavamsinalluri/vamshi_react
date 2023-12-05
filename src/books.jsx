b4e3159d887fab80dc259fea3e515701c14192ef
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-12-05 16:55:25 +0530

    ss

---
+++ src/books.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+import axios from "axios"
+function Data(){
+    var [books,setBooks]=React.useState([])
+    var [newbook,setNewbook]=React.useState({title:"",auther:""})
+  React.useEffect(()=>{
+    getbooks()
+   },[])
+    function addbook(){
+        axios.post("http://localhost:4000/students",newbook).then((res)=>{
+            console.log(res)
+            getbooks()
+        })
+    }
+    function getbooks(){
+        axios.get("http://localhost:4000/students").then((res)=>{console.log(res.data)
+        setBooks([...res.data])
+    })
+    }
+    function del(){
+        
+    }
+   return(
+    
+        <div>
+            <input type="text" onChange={(event)=>{setNewbook({...newbook,title:event.target.value})}}/><br></br>
+            <input type="text" onChange={(event)=>{setNewbook({...newbook,auther:event.target.value})}}/>
+            <br>
+            </br>
+            <button onClick={addbook}>add book</button>
+            {
+                books.map((a)=>{
+                    return(<div className="box">
+                    <div className="content">
+                    <h1>{a.title}</h1>
+                        <b>{a.auther}</b>
+                        <button onClick={del}>delete</button>
+                    </div>
+                        </div>)
+                })
+            }
+        </div>
+    )
+}
+export default Data
