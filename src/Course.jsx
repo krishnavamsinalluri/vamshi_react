d6fc5d231bec3f1d7195d2d4a0daaba54810a515
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-12-07 11:51:42 +0530

    ss

--- src/Course.jsx
+++ src/Course.jsx
@@ -20,23 +20,40 @@ function Canner() {
                 }
             </div>
             <div>
-                <div id="carouselExample" class="carousel slide">
+                <div id="carouselExampleCaptions" class="carousel slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img  src="https://www.whereig.com/india/states/map/andhra-pradesh-district-map.jpg" class="d-block w-100" alt="1st" />
+                            <img src="https://diligentias.com/wp-content/uploads/2019/05/Map-of-Andhra-pradesh.png" class="d-block w-100" alt="1st" />
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>First slide label</h5>
+                                    <p>Some representative placeholder content for the first slide.</p>
+                                </div>
                         </div>
                         <div class="carousel-item">
-                            <img src="https://www.toptourguide.com/indialarge.gif" class="d-block w-100" alt="2nd" />
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/c/c1/AndhraPradesh_state_map.jpg" class="d-block w-100" alt="2nd" />
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Second slide label</h5>
+                                    <p>Some representative placeholder content for the second slide.</p>
+                                </div>
                         </div>
                         <div class="carousel-item">
-                            <img src="https://telangana.gov.in/PublishingImages/Pages/Home/Telangana%20Map-HomePage-33districts.png" class="d-block w-100" alt="3rd" />
+                            <img src="https://apedb.gov.in/assets/images/about-ap/aboutapmapsvg.svg" class="d-block w-100" alt="3rd" />
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Third slide label</h5>
+                                    <p>Some representative placeholder content for the third slide.</p>
+                                </div>
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
@@ -48,4 +65,4 @@ function Canner() {
 
 
 }
-export default Canner
+export default Canner;
