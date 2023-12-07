4d36cf2c07843deb496224de4122e552785d8793
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-12-07 12:40:01 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Forms from './Studentsfroms';
 import  Form from "./Myform"
+import Data from './Froms';
 
 function App() {
   return (
     <div className="box">
       <Forms></Forms>
      <Form></Form>
+     <Data></Data>
 
  </div>
   );
---
+++ src/Froms.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Formik, useFormik } from "formik";
+import *as Yup from "yup"
+function Data(){
+    var from=useFormik({
+        initialValues:{ email: '', password: '' },
+        onSubmit:(values)=>{
+            console.log(values)
+        },
+    
+    validationSchema: Yup.object({
+        email: Yup.string()
+          .max(15, 'Must be 15 characters or less')
+          .required('Required'),
+        password: Yup.string()
+          .max(20, 'Must be 20 characters or less')
+          .required('Required')
+        
+      })
+})
+
+
+
+return (
+    <div>
+    <form onSubmit={from.handleSubmit}>
+      Email:  <input type="text"  onSubmit={from.handleSubmit}/>
+        <b>{from.touched.email && from.errors && from.errors.email}</b> <br />
+     password:   <input type="text"  onSubmit={from.handleSubmit}/>
+        <b>{from.touched.password && from.errors && from.errors.password}</b>
+
+
+    </form>
+    </div>
+)
+}
+export default Data
