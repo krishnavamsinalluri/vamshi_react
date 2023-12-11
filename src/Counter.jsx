b2f676485367c715d1baa8a6cec6f875fb77ca68
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-12-11 12:40:30 +0530

    ss

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,11 +1,25 @@
-import React from "react"
-function Counter(props){
+import React from "react";
+import { connect } from "react-redux";
+import {incerimentCounter, ResetCounter, decrimentCounter } from "./store/actions";
+function Counters(props){
     console.log(props)
     return(
-        <div>
-            <h1> count{props.c.count}</h1>
-            
+        <div className="box">
+            <h1>Counter:{props.Counter}</h1>
+            <button onClick={()=>{props.incer()}}>Inceriment</button>
+            <button onClick={()=>{props.decr()}}>Deceriment</button>
+            <button onClick={()=>{props.rese()}}>Reset</button>
         </div>
     )
 }
-export default Counter
+
+    function mapStateToProps(state){return  state.c}
+    function mapDispatchToProps(dispatch)
+    {return  {
+        incer:()=>{dispatch(incerimentCounter())},
+        decr:()=>{dispatch(decrimentCounter())},
+        rese:()=>{dispatch(ResetCounter())}
+     }
+    }         
+
+export default connect(mapStateToProps,mapDispatchToProps)(Counters)
