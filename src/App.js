ad844f1e1769b02bf2ab1ad67e9b0594f9703ae0
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-12-11 11:31:12 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
 import store from "./Store";
+import { Provider } from "react-redux";
+import Counter from "./counter";
+function App(){
+
+  return(
+    <div>
+      <Provider store={store}>
+        <Counter></Counter>
+      </Provider>
+    </div>
+  )
+}
+export default App
---
+++ src/counter.js
@@ -0,0 +1,10 @@
+import React from "react"
+function Counter(props){
+    console.log(props)
+    return(
+        <div>
+            <h1></h1>
+        </div>
+    )
+}
+export default Counter
