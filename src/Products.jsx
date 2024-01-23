7771143aec32bf86ca6ee8a99e3748409b357412
krishnavamsinalluri <vamsinalluri806@gmail.com>
2024-01-23 11:25:17 +0530

    ss

---
+++ src/Products.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Product(){
+    var [data,setData]=React.useState()
+    axios.get("")
+    return (
+            <div>
+
+            </div>
+    )
+}
+export default Product
