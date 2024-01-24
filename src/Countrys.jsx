105d22e0a649f9e037190407a0f7d37b7411f54f
krishnavamsinalluri <vamsinalluri806@gmail.com>
2024-01-24 12:34:03 +0530

    ss

---
+++ src/Countrys.jsx
@@ -0,0 +1,13 @@
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+function Country(){
+    var[data,setData]=useState([])
+    useEffect(()=>{
+        axios.get('')
+    },[])
+    return(
+        <div></div>
+    )
+
+}
+export default Country
--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<div></div>,
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router} />
+
     <App />
   </React.StrictMode>
 );
