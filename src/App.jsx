51132f2a6508a1590571bbfb945ce128bc7eeb39
krishnavamsinalluri <vamsinalluri806@gmail.com>
2024-01-27 10:07:41 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 function App() { 
   return (
-    <div className="Box">
-    
-       </div>
-  );
+      <div className="box">
+        <h1>vamsai</h1>      
+      </div>
+
+ );
 }
 
 export default App;
